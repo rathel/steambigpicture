a4941860d18c082401b6ae4e088856ca3ffc6feb
rathel <rathel@gmail.com>
2021-12-28 16:55:40 -0700

    changed wording and added display

--- bigpicture.sh
+++ bigpicture.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+export DISPLAY=:0
+
 args=$1
 
-if [[ "$args" == "off" ]]; then
+if [[ "$args" == "bp" ]]; then
     pkill steam
     sleep 5s && ./disable.sh
     sleep 5s && steam -tenfoot > /dev/null 2>&1 &
 fi
-if [[ "$args" == "on" ]]; then
+if [[ "$args" == "si" ]]; then
     pkill steam
     sleep 5s && ./enable.sh
     sleep 5s && steam -silent > /dev/null 2>&1 &
